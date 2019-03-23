042a543650732cd588a420efc799f7252cb4ce10
Sam Pop <poplovitch.samuel@gmail.com>
2019-03-23 13:28:38 -0400

    Updates

---
+++ precache-manifest.52b99a41985d207e2df460973997d95e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de75c0bbad0d8ebaaeb3",
+    "url": "/landingPage/static/css/main.93f5b5c0.chunk.css"
+  },
+  {
+    "revision": "de75c0bbad0d8ebaaeb3",
+    "url": "/landingPage/static/js/main.1cf01c07.chunk.js"
+  },
+  {
+    "revision": "6c20253fe316bbc606c6",
+    "url": "/landingPage/static/js/runtime~main.e6768711.js"
+  },
+  {
+    "revision": "73f558aa28f1424e78ad",
+    "url": "/landingPage/static/js/2.dd51780c.chunk.js"
+  },
+  {
+    "revision": "263372dacca17140344bf6a8737b760b",
+    "url": "/landingPage/index.html"
+  }
+];
